1b3f44247c66868bd97985187f1400f3fe84328c
llamicron <llamicron@gmail.com>
2020-11-15 13:23:05 -0600

    Adds the basics

---
+++ docs/main.js
@@ -0,0 +1,12 @@
+
+var app = new Vue({
+    el: "#content",
+    data: {
+        classes: ["PHYS 1444", "CSE 1325", "CSE 2315", "MATH 2425"]
+    },
+    methods: {
+        setActiveClass(cls) {
+            console.log("Nothing yet");
+        }
+    }
+})
